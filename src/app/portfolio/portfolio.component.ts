570da4e331f31a0c1be161a684cb32ea04beed9c
anilorhan -fwfac <orhan_anil@yahoo.de>
2023-04-21 11:46:59 +0200

    made on hover project box dynamic

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,33 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  onOverlay = false;
-
   projects = [
     {
-      image: 'link',
+      image: 'assets/img/projects/el-pollo-loco.png',
+      imageByZoom: 'assets/img/projects/el-pollo-loco-bg.png',
       title: 'El Pollo Loco',
-      description: 'Jump and run game',
-      type: 'Angular',
-      link: 'link',
-      github: 'link'
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen',
+      type: 'JavaScript | HTML | CSS',
+      link: 'http://anil-orhan.developerakademie.net/el_pollo_loco/index.html',
+      github: 'https://github.com/anil-o/elPolloLoco',
+      isHovered: false,
+    },
+    {
+      image: 'assets/img/projects/join-with-laptop.png',
+      imageByZoom: 'assets/img/projects/join.png',
+      title: 'Join',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasking using drag and drop functions assign users and categories.',
+      type: 'JavaScript | HTML | CSS',
+      link: 'https://anil-orhan.developerakademie.net/join/index.html',
+      github: 'https://github.com/anil-o/join',
+      isHovered: false,
     },
   ];
   ngOnInit(): void {
 
   }
 
-  onOverlayTrue() {
-    if (!this.onOverlay) {
-      this.onOverlay = true;
-      console.log('Function onOverlayTrue calling', this.onOverlay);
+  onOverlayTrue(index: number) {
+    if (!this.projects[index].isHovered) {
+      this.projects[index].isHovered = true;
+      console.log('on box: Should be true', this.projects[index].isHovered);
     }
   }
 
-  onOverlayFalse() {
-    if (this.onOverlay) {
-      this.onOverlay = false;
-      console.log('Function onOverlayTrue calling', this.onOverlay);
+  onOverlayFalse(index: number) {
+    if (this.projects[index].isHovered) {
+      this.projects[index].isHovered = false;
+      console.log('onleave box: Should be false', this.projects[index].isHovered);
     }
   }
 }
