07149ccca871a8aa07648422aeb54af101690985
anilorhan -fwfac <orhan_anil@yahoo.de>
2023-05-03 11:27:37 +0200

    responsive menu added

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  isMenuOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  toggleMenu() {
     
   }
   
