8d5515c1d7284285e6f65f534a0f54d3156bc6d3
anilorhan -fwfac <orhan_anil@yahoo.de>
2023-05-20 07:56:00 +0200

    validators implementen in contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,64 +1,59 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent implements OnInit{
-@ViewChild('myForm') myForm: ElementRef
-@ViewChild('name') name: ElementRef
-@ViewChild('message') email: ElementRef
-@ViewChild('sendMessage') message: ElementRef
+export class ContactComponent implements OnInit {
+  @ViewChild('contact') contact: ElementRef
 
-animation = false;
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+  });
 
-ngOnInit(): void {
-  
-}
+  name: any;
+  email: any;
+  message: any;
+  animation = false;
 
 
-async sendMail() {
-  let name = this.name.nativeElement;//get the id like in JS
-  let email = this.email.nativeElement;
-  let message = this.message.nativeElement;
-  let myForm = this.myForm.nativeElement;
+  constructor() { }
 
-  let formData = new FormData();
-  formData.append('name', name.value);
-  formData.append('email', email.value);
-  formData.append('message', message.value);
-  
-  //send 
-  await fetch('https://anil-orhan.developerakademie.net/send_mail/send_mail.php',
-  {
-    method: 'POST',
-    body: formData
+  ngOnInit(): void {
+
   }
-  );
-  name.disabled = true;
-  email.disabled = true;
-  message.disabled = true;
-  this.emailsent(name, email, message, myForm);
-}
 
-emailsent(name, email, message, myForm) {
-  this.animation = true;
-  myForm.reset();
-  name.disabled = false;
-  email.disabled = false;
-  message.disabled = false;
+  async sendMail() {
+    let formData = new FormData();
+    formData.append('name', this.name);
+    formData.append('email', this.email);
+    formData.append('message', this.message);
 
-  setTimeout(() => {
-    this.animation = false;
-  }, 2000);
-}
+    //send
+    await fetch('https://anil-orhan.developerakademie.net/send_mail/send_mail.php',
+      {
+        method: 'POST',
+        body: formData
+      }
+    );
+    this.emailSent();
+  }
+
+  emailSent() {
+    this.animation = true;
+    let contact = this.contact.nativeElement;
+    contact.reset();
+    setTimeout(() => {
+      this.animation = false;
+    }, 2000);
+  }
 }
